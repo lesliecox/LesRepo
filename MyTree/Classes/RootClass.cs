87a0550b49f770d122d72751fa8b8c1e8e8a3269
leslie cox <leslie.cox@gmx.co.uk>
2020-05-28 12:23:25 +0100

    Add project files.

---
+++ MyTree/Classes/RootClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTree.Classes
+{
+    public class RootClass
+    {
+        public string FullPath;
+        public object Type;  
+
+        public RootClass()
+        {
+
+        }
+        public enum Items
+        {
+            File,
+            Folder,
+            Drive
+        }
+
+        public static List<Items> GetLogicalDrives()
+        {
+            var FullPath = "null";
+            var Type = "null";
+
+            return Directory.GetLogicalDrives().Select(Drive => new Items {FullPath = Drive, Type = Items.Drive }).ToList();
+
+        }
+
+
+
+
+
+
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            var normalizedPath = path.Replace('/', '\\');
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+            if (lastIndex <= 0)
+                return path;
+            return path.Substring(lastIndex + 1);
+
+        }
+
+
+    }
+
+}       
+
+        
+
